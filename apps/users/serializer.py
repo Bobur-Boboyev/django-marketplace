506f21e21380e370e6a96f5124b8d34062ab7d3b
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-05-09 00:37:34 +0500

    Refactor UserSerializer to use serializers.Serializer and update user registration URLs

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,15 +1,21 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from django.contrib.auth.hashers import make_password
+from rest_framework.validators import UniqueValidator
+from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'password', 'role']
-        extra_kwargs = {
-            'password': {'write_only': True},
-            'is_staff': {'read_only': True},
-        }
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all(), message="Email already exists.")]
+    )
+    ROLE_CHOICES = (
+        ('vendor', 'Vendor'),
+        ('customer', 'Customer'),
+    )
+    role = serializers.ChoiceField(choices=ROLE_CHOICES, default='customer')
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
+    phone = serializers.CharField(required=False, allow_blank=True)
+    password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
@@ -18,9 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
         password = validated_data.pop('password', None)
         if password:
             instance.set_password(password)
-        
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        
         instance.save()
         return instance
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,11 +4,16 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from apps.users.views import LogoutView
+from apps.users.views import LogoutView, register_user, user_profile, list_users
 
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/logout/', LogoutView.as_view(), name='auth_logout'),
+
+    path('api/users/register/', register_user, name='register'),
+    path('api/users/profile/', user_profile, name='profile'),
+
+    path('api/users/', list_users, name='list_users')
 ]
