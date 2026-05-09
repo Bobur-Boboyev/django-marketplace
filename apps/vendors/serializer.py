1e2bc4f9fd356bfb74bdeb1629947bce294d7bb4
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-05-09 12:53:03 +0500

    Add VendorSerializer for handling Vendor model data and slug generation

---
+++ apps/vendors/serializer.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.utils.text import slugify
+
+from .models import Vendor
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = "__all__"
+        read_only_fields = ("id", "slug", "is_active", "created_at", "updated_at", "status", "user", "is_verified", "is_featured")
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+
+        base_slug = slugify(validated_data['name'])
+        slug = base_slug
+        counter = 1
+
+        while Vendor.objects.filter(slug=validated_data['slug']).exists():
+            validated_data['slug'] = f"{slugify(validated_data['name'])}-{counter}"
+            slug = validated_data['slug'] + "-" + str(counter)
+            counter += 1
+
+        validated_data['user'] = user
+        validated_data['slug'] = slugify(validated_data['name'])
+
+        return super().create(validated_data)
+    
