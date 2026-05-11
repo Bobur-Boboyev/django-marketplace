1887ca6557ea2d80028047c1fe776c2181083d8a
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-05-11 16:33:12 +0500

    Add CartItemSerializer for CartItem model serialization

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -15,9 +15,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
 
     product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
-
     quantity = models.PositiveIntegerField(default=1)
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
---
+++ apps/cart/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Cart, CartItem
+from apps.products.models import Product
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity"]
