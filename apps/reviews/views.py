8afe55cb7c6beb30fb2bed9b93e1bfa5f08785ce
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-05-15 22:15:00 +0500

    Implement ReviewViewSet for managing reviews with authentication permissions

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from apps.reviews.models import Review
+from apps.reviews.serializers import ReviewSerializer
+from apps.products.models import Product
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.select_related(
+        'user',
+        'product'
+    )
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
