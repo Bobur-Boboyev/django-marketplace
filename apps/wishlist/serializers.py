36d9271eaf040a1c998b24d90567074eabb877e7
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-05-15 22:24:51 +0500

    Add WishlistSerializer for Wishlist model with product details

---
+++ apps/wishlist/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from apps.wishlist.models import Wishlist
+from apps.products.serializer import ProductSerializer
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    product_detail = ProductSerializer(
+        source='product',
+        read_only=True
+    )
+
+    class Meta:
+        model = Wishlist
+        fields = [
+            'id',
+            'product',
+            'product_detail',
+            'created_at'
+        ]
